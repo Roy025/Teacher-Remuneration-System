a44d19239eb01bfad12edd51b11baf5bcf84a2ab
nihalshahria <nihalshahria@gmail.com>
2022-09-15 18:15:59 +0600

    backend program.cs structure modified

---
+++ backend/Api/Program.cs
@@ -0,0 +1,19 @@
+using Api.Startup;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApplicationServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.ConfigureSwagger();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
