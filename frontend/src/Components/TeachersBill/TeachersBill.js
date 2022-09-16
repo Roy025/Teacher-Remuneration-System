691f274dd569a99b8f767baa07455620cba5f170
raisul191491 <aupiaupu@gmail.com>
2022-09-16 22:03:56 +0600

    Starting landing page creation

---
+++ frontend/src/Components/TeachersBill/TeachersBill.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Navbar, NavbarItems} from "../Navbar"
+import "../Navbar/Navbar.css"
+
+function TeachersBill (){
+    return(
+        <Navbar></Navbar>
+    )
+}
