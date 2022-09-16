b2a9f3d5ad93e63bbc3061ff9668704b36378527
nihalshahria <nihalshahria@gmail.com>
2022-09-17 01:24:30 +0600

    Add and Configure StoreContext (database)

---
+++ backend/Business/Data/StoreContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Data;
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+    {
+    }
+
+    // public DbSet<{{Entity}}> {{TableName}} { get; set; } = null;
+}
