46f10111ea7caadec66f115ee5a7f8a7b710fcf0
raisul191491 <aupiaupu@gmail.com>
2022-09-17 00:25:48 +0600

    Navbar change

---
+++ frontend/src/Components/NavbarInside/NavbarInsideItems.js
@@ -0,0 +1,17 @@
+export const Items = [
+    {
+        title:  "FAQ",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title:  "Notifications",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title:  "Profile",
+        url: "#",
+        cName: "nav-links"
+    }
+]
