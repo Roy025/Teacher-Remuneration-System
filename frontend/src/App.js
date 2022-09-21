78ed822b153f94db1b4b11200949729252391f3f
raisul191491 <aupiaupu@gmail.com>
2022-09-21 16:23:40 +0600

    Frontend base update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavbarInside from "./Components/NavbarInside/NavbarInside";
 import { Routes, Route } from "react-router-dom";
+
+//Components
+import NavbarInside from "./Components/NavbarInside/NavbarInside";
 import LandingPage from "./Components/LandingPage/LandingPage";
+import TeachersBill from "./Components/TeachersBill/TeachersBill"
 import FAQ from "./Components/FAQ/FAQ";
+import ProfilePage from "./Components/ProfilePage/ProfilePage";
+import Notifications from "./Components/Notifications/Notifications"
 
 function App() {
   return (
     <>
       <NavbarInside></NavbarInside>
       <Routes>
-        <Route path="/" element={<LandingPage/>}></Route>
-        <Route path="/faq" element={<FAQ/>}></Route>
+        <Route exact path="/" element={<LandingPage/>}></Route>
+        <Route exact path="/faq" element={<FAQ/>}></Route>
+        <Route exact path="/teachersbill" element={<TeachersBill/>}> </Route>
+        <Route exact path="/profile" element={<ProfilePage/>}></Route>
+        <Route exact path="/notifications" element={<Notifications/>}></Route>
       </Routes>
     </>
   );
