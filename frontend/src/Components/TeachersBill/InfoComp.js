59dc8dae43c343c3c9bf3e5ddb8f3831713a831e
Roy025 <prapti.roy025@gmail.com>
2022-09-22 23:07:55 +0600

    Teacherbill

--- frontend/src/Components/TeachersBill/InfoComp.js
+++ frontend/src/Components/TeachersBill/InfoComp.js
@@ -3,49 +3,49 @@ import React from 'react';
 const InfoComp = () => {
 	return (
 		<div className="row">
-			<div class="form-group col-md-3">
-				<label for="exampleFormControlInput1">Institute</label>
+			<div className="form-group col-md-3">
+				<label>Institute</label>
 				<input
 					name="institute"
 					type="text"
-					class="form-control"
+					className="form-control"
 					id="institute"
 				/>
 			</div>
-			<div class="form-group col-md-3">
-				<label for="exampleFormControlInput1">Department</label>
+			<div className="form-group col-md-3">
+				<label>Department</label>
 				<input
 					name="department"
 					type="text"
-					class="form-control"
+					className="form-control"
 					id="department"
 				/>
 			</div>
-			<div class="form-group col-md-3">
-				<label for="exampleFormControlInput1">Teacher Name</label>
+			<div className="form-group col-md-3">
+				<label>Teacher Name</label>
 				<input
-					name="member"
+					name="name"
 					type="text"
-					class="form-control"
-					id="member"
+					className="form-control"
+					id="name"
 				/>
 			</div>
-			<div class="form-group col-md-2">
-				<label for="exampleFormControlInput1">CourseID</label>
+			<div className="form-group col-md-2">
+				<label>CourseID</label>
 				<input
 					name="course"
 					type="text"
-					class="form-control"
+					className="form-control"
 					id="course"
 				/>
 			</div>
-			<div class="col-md-1">
+			<div className="col-md-1">
 				<button
 					type="button"
-					class="add"
+					className="add"
 					//onClick={addFields}
 				>
-					<i class="fa-sharp fa-solid fa-plus"></i>
+					<i className="fa-sharp fa-solid fa-plus"></i>
 				</button>
 			</div>
 		</div>
--- frontend/src/Components/TeachersBill/TeacherBill.js
+++ frontend/src/Components/TeachersBill/TeacherBill.js
@@ -1,7 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../SampleDropdown/styles.css';
 
 const TeacherBill = () => {
-	return <div>TeacherBill</div>;
+	const [inputFields, setInputFields] = useState([
+		{
+			institute: '',
+			department: '',
+			name: '',
+			course: '',
+		},
+	]);
+
+	const addInputField = (e) => {
+		e.preventDefault();
+		setInputFields([
+			...inputFields,
+			{
+				institute: '',
+				department: '',
+				name: '',
+				course: '',
+			},
+		]);
+	};
+	const removeInputFields = (index, e) => {
+		e.preventDefault();
+		const rows = [...inputFields];
+		rows.splice(index, 1);
+		setInputFields(rows);
+	};
+	const handleChange = (index, evnt) => {
+		const { name, value } = evnt.target;
+		const list = [...inputFields];
+		list[index][name] = value;
+		setInputFields(list);
+	};
+	return (
+		<div className="container">
+			{inputFields.map((data, index) => {
+				const { institute, department, name, course } = data;
+				return (
+					<div
+						className="row"
+						key={index}>
+						<div className="form-group col-md-3">
+							<label>Institute</label>
+							<input
+								type="text"
+								name="institute"
+								onChange={(evnt) => handleChange(index, evnt)}
+								value={institute}
+								className="form-control"
+								id="institute"
+								placeholder="Institute"
+							/>
+						</div>
+						<div className="form-group col-md-3">
+							<label>Department</label>
+							<input
+								type="text"
+								name="department"
+								onChange={(evnt) => handleChange(index, evnt)}
+								value={department}
+								className="form-control"
+								id="department"
+								placeholder="Department"
+							/>
+						</div>
+						<div className="form-group col-md-3">
+							<label>Teacher Name</label>
+							<input
+								type="text"
+								name="name"
+								onChange={(evnt) => handleChange(index, evnt)}
+								value={name}
+								className="form-control"
+								id="name"
+								placeholder="Name"
+							/>
+						</div>
+						<div className="form-group col-md-2">
+							<label>CourseID</label>
+							<input
+								type="text"
+								name="course"
+								onChange={(evnt) => handleChange(index, evnt)}
+								value={course}
+								className="form-control"
+								id="name"
+								placeholder="Course"
+							/>
+						</div>
+
+						<div className="col-md-1 text-center">
+							{inputFields.length !== 1 ? (
+								<button
+									className="btn-outline-danger add"
+									onClick={removeInputFields}>
+									x
+								</button>
+							) : (
+								''
+							)}
+						</div>
+					</div>
+				);
+			})}
+
+			<div className="row">
+				<div className="col-sm-12">
+					<button
+						className="btn btn-outline-success "
+						onClick={addInputField}>
+						Add New
+					</button>
+				</div>
+			</div>
+			<div className="col-sm-4"></div>
+		</div>
+	);
 };
 
 export default TeacherBill;
