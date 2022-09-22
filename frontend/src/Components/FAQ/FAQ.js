78d27f6cb5c76acb84ef81e42f2a23ee90aae2cf
raisul191491 <aupiaupu@gmail.com>
2022-09-22 16:01:13 +0600

    FaQ update

--- frontend/src/Components/FAQ/FAQ.js
+++ frontend/src/Components/FAQ/FAQ.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./FAQ.css";
+import Questions from "./Questions";
 
 function FAQ() {
   return (
     <div>
-      This is FAQ
+      <div className="Main-container">
+        <h1 className="Headline">সাধারণ জিজ্ঞাসাসমূহ</h1>
+        <p className="Top-text">সমস্যার সম্মুখীন হলে উত্তরণ পাবেন এখানেই!</p>
+        <form action="" className="Search-bar">
+          <input
+            type="text"
+            placeholder="প্রশ্ন খোঁজ করুন ..."
+            name="Search-bar"
+          />
+          <button type="submit">
+            <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+          </button>
+        </form>
+          <Questions />
+      </div>
     </div>
-  )
+  );
 }
 
-export default FAQ
+export default FAQ;
---
+++ frontend/src/Components/FAQ/Questions.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Questions.css"
+
+function Questions() {
+  return (
+    <div className='Main-container'>
+      <div className="Question">
+        <h5 >How to create an account?</h5>
+        <i class="fa-sharp fa-solid fa-caret-down"></i>
+      </div>
+      <div className="Answer"></div>
+    </div>
+  )
+}
+
+export default Questions
