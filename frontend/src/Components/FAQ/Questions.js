ffd5702b1f37e4070b4bc94e1f95f7997a838797
raisul191491 <aupiaupu@gmail.com>
2022-09-22 17:41:59 +0600

    dropdown faq continue

--- frontend/src/Components/FAQ/Questions.js
+++ frontend/src/Components/FAQ/Questions.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import "./Questions.css"
+import { useState } from "react"
 
 function Questions() {
+
+  const[click, setClick] = useState("false");
+
+  function changeView(){
+    setClick(!click)
+  }
+
   return (
-    <div className='Main-container'>
+    <div className='Faq-container'>
       <div className="Question">
         <h5 >How to create an account?</h5>
         <i class="fa-sharp fa-solid fa-caret-down"></i>
       </div>
-      <div className="Answer"></div>
+      <div className= {click ? "Answer Clicked" : "Answer"} onClick={changeView}>
+        <p>Like my father did.</p>
+      </div>
     </div>
   )
 }
