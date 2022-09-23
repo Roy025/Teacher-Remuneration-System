a42bb66590e22299610b3cfb7cee303ddc0c3917
Roy025 <prapti.roy025@gmail.com>
2022-09-24 04:47:09 +0600

    Add

--- frontend/src/Components/TeachersBill/ChiefInvigilatorBill.js
+++ frontend/src/Components/TeachersBill/ChiefInvigilatorBill.js
@@ -8,16 +8,9 @@ import Dropdown, {
 	sessionOptions,
 } from '../SampleDropdown/Dropdown';
 import '../SampleDropdown/styles.css';
-import InfoComp from './InfoComp';
+import TeacherBill from './TeacherBill';
 
 const ChiefInvigilatorBill = () => {
-	const [inputFields, setInputFields] = useState([
-		{ institute: '', department: '', member: '' },
-	]);
-	const addFields = () => {
-		let newfield = { institute: '', department: '', member: '' };
-		setInputFields([...inputFields, newfield]);
-	};
 	return (
 		<>
 			<div className="row">
@@ -25,39 +18,30 @@ const ChiefInvigilatorBill = () => {
 				<form className="col-md-10">
 					<h1 className="text-center">Chief Invigilator Bill</h1>
 					<div className="row">
-						<div class="form-group col-md-5">
+						<div className="form-group col-md-5">
 							<Dropdown
 								options={semesterOptions}
 								dropdownTitle={semesterTitle}
 							/>
 						</div>
-						<div class="form-group col-md-5">
+						<div className="form-group col-md-5">
 							<Dropdown
 								options={sessionOptions}
 								dropdownTitle={sessionTitle}
 							/>
 						</div>
 					</div>
-					<div class="form-group col-md-4">
-						<label for="exampleFormControlInput1">CourseID</label>
+					<div className="form-group col-md-4">
+						<label>CourseID</label>
 						<input
 							name="course"
 							type="text"
-							class="form-control"
+							className="form-control"
 							id="course"
 						/>
 					</div>
 					<h5>Invigilator</h5>
-					<InfoComp />
-					<div class="col-md-1">
-						<button
-							type="button"
-							class="add"
-							//onClick={addFields}
-						>
-							<i class="fa-sharp fa-solid fa-plus"></i>
-						</button>
-					</div>
+					<TeacherBill />
 				</form>
 			</div>
 		</>
