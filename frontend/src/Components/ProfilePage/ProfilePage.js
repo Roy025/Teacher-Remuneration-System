8caddc3408e36d0cd5d33cae0c7771a36eff30ac
raisul191491 <aupiaupu@gmail.com>
2022-09-24 19:47:41 +0600

    Profile page update

--- frontend/src/Components/ProfilePage/ProfilePage.js
+++ frontend/src/Components/ProfilePage/ProfilePage.js
@@ -12,25 +12,66 @@ function ProfilePage() {
 
   return (
     <div className="MainProfileBody">
+      {/* Profile avatar and calendar */}
       <div className="ProfileHeadContainer">
         <div className="ProfileHead">
           <div className="ProfileAvatar">
-            <img src={require("./images/profilePic.jpg")} alt="profilePic" />
+            <img
+              className="ProfilePic"
+              src={require("./images/profilePic.jpg")}
+              alt="profilePic"
+            />
           </div>
           <div className="NameAndInstitution">
             <div className="ProfileName">Raihan Ullah</div>
             <div className="ProfileInstitution">
               Shahjalal University of Science and Technology
             </div>
-            <div className="ProfileDesignation">
-              Lecturer
-            </div>
+            <div className="ProfileDesignation">Lecturer</div>
           </div>
         </div>
         <div className="ProfileCalender">
-          <DatePicker onChange={onChange} value={date} />
+          <DatePicker
+            onChange={onChange}
+            value={date}
+            className="Calendar-design"
+            weekStartsFrom="Sunday"
+          />
         </div>
       </div>
+
+      {/* Dashboard */}
+      <div className="Summary-dashboard">
+        <h3 className="Summary">Summary</h3>
+        
+        <div className="Money-dashboard">
+          <div className="Money-card">
+            <h3 className="Money-card-title">Total Income</h3>
+            <h5 className="Money-card-amount">BDT 4,49,650</h5>
+          </div>
+          <div className="Money-card Withdrwan">
+            <h3 className="Money-card-title">Withdrwan Amount</h3>
+            <h5 className="Money-card-amount">BDT 4,00,650</h5>
+          </div>
+          <div className="Money-card Remaining">
+            <h3 className="Money-card-title">Remaining Amount</h3>
+            <h5 className="Money-card-amount">BDT 49,650</h5>
+          </div>
+          <div className="Money-card Committee">
+            <h3 className="Money-card-title">Active Committee</h3>
+            <h5 className="Money-card-amount">02</h5>
+          </div>
+        </div>
+      </div>
+
+      {/* Profile info */}
+      <div className="Profile-info">
+        <div className="Cleared-voucher">
+          <h1 className="Cleared-voucher-heading">Cleared Vouchers</h1>
+        </div>
+        <div className="Uncleared-voucher"></div>
+      </div>
+
     </div>
   );
 }
