3e47a753004cf7802c72f94c6ba2fd74f60b61da
Roy025 <prapti.roy025@gmail.com>
2022-09-24 18:49:30 +0600

    Chief_ivni badh rakh

--- frontend/src/Components/ProfilePage/ProfilePage.js
+++ frontend/src/Components/ProfilePage/ProfilePage.js
@@ -1,38 +1,42 @@
-import React from "react";
-import "./ProfilePage.css";
-import DatePicker from "sassy-datepicker";
-import { useState } from "react";
+import React from 'react';
+import './ProfilePage.css';
+import DatePicker from 'sassy-datepicker';
+import { useState } from 'react';
 
 function ProfilePage() {
-  const [date, setDate] = useState(new Date());
+	const [date, setDate] = useState(new Date());
 
-  const onChange = (date) => {
-    setDate(date);
-  };
+	const onChange = (date) => {
+		setDate(date);
+	};
 
-  return (
-    <div className="MainProfileBody">
-      <div className="ProfileHeadContainer">
-        <div className="ProfileHead">
-          <div className="ProfileAvatar">
-            <img src={require("./images/profilePic.jpg")} alt="profilePic" />
-          </div>
-          <div className="NameAndInstitution">
-            <div className="ProfileName">Raihan Ullah</div>
-            <div className="ProfileInstitution">
-              Shahjalal University of Science and Technology
-            </div>
-            <div className="ProfileDesignation">
-              Lecturer
-            </div>
-          </div>
-        </div>
-        <div className="ProfileCalender">
-          <DatePicker onChange={onChange} value={date} />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="MainProfileBody">
+			<div className="ProfileHeadContainer">
+				<div className="ProfileHead">
+					<div className="ProfileAvatar">
+						<img
+							src={require('./images/profilePic.jpg')}
+							alt="profilePic"
+						/>
+					</div>
+					<div className="NameAndInstitution">
+						<div className="ProfileName">Raihan Ullah</div>
+						<div className="ProfileInstitution">
+							Shahjalal University of Science and Technology
+						</div>
+						<div className="ProfileDesignation">Lecturer</div>
+					</div>
+				</div>
+				<div className="ProfileCalender">
+					<DatePicker
+						onChange={onChange}
+						value={date}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProfilePage;
