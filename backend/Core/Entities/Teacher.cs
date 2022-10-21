32d75d1b332fb21f4dd3c45d343b3a86a50f18dc
nihalshahria <nihalshahria@gmail.com>
2022-10-22 00:18:38 +0600

    Update TeacherEntity

--- backend/Core/Entities/Teacher.cs
+++ backend/Core/Entities/Teacher.cs
@@ -2,6 +2,7 @@ namespace Core.Entities;
 public class Teacher : BaseEntity
 {
     public string Name { get; set; }
+    public string Institute { get; set; }
     public string Department { get; set; }
     public string BankAccount { get; set; } = null!;
     public string Designation { get; set; } = "Lecturer";
