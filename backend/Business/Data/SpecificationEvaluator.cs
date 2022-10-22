0fc4b26ff47495e0774c8f9e0e0f9a228d1b2275
nihalshahria <nihalshahria@gmail.com>
2022-10-23 00:36:33 +0600

    Setup GenericRepository and UnitOfWork

---
+++ backend/Business/Data/SpecificationEvaluator.cs
@@ -0,0 +1,39 @@
+using Core.Entities;
+using Core.Interfaces.Repositories;
+
+namespace Business.Data;
+public static class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+{
+    public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+    {
+        var query = inputQuery;
+
+        if (spec.Criteria != null)
+        {
+            query = query.Where(spec.Criteria);
+        }
+
+        if (spec.OrderBy != null)
+        {
+            query = query.OrderBy(spec.OrderBy);
+        }
+
+        if (spec.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(spec.OrderByDescending);
+        }
+
+        if (spec.GroupBy != null)
+        {
+            query = query.GroupBy(spec.GroupBy).SelectMany(x => x);
+        }
+
+        if (spec.IsPagingEnabled)
+        {
+            query = query.Skip(spec.Skip).Take(spec.Take);
+        }
+
+        query = spec.Includes.Aggregate(query, (current, include) => include(current));
+        return query;
+    }
+}
---
+++ backend/Core/Interfaces/Repositories/ISpecification.cs
@@ -0,0 +1,16 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Core.Interfaces.Repositories;
+public interface ISpecification<T>
+{
+    Expression<Func<T, bool>>? Criteria { get; }
+    // List<Expression<Func<T, object>>> Includes { get; }
+    List<Func<IQueryable<T>, IIncludableQueryable<T, object>>> Includes { get; }
+    Expression<Func<T, object>>? OrderBy { get; }
+    Expression<Func<T, object>>? OrderByDescending { get; }
+    Expression<Func<T, object>>? GroupBy { get; }
+    int Take { get; }
+    int Skip { get; }
+    bool IsPagingEnabled { get; }
+}
