d6be52383799625fa01b5a8338169d3813a49e6b
raisul191491 <aupiaupu@gmail.com>
2022-10-28 14:34:28 +0600

    .

--- frontend/src/Components/TeachersBill/ChiefInvigilatorForm.js
+++ frontend/src/Components/TeachersBill/ChiefInvigilatorForm.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Dropdown, {
+	semesterOptions,
+	semesterTitle,
+	sessionTitle,
+	sessionOptions,
+} from '../SampleDropdown/Dropdown';
 
 function ChiefInvigilatorForm() {
   return (
-    <div>ChiefInvigilatorForm</div>
+    <div>
+      <div className="row">
+						<div className="form-group col-md-5">
+							<Dropdown
+								options={semesterOptions}
+								dropdownTitle={semesterTitle}
+							/>
+						</div>
+						<div className="form-group col-md-5">
+							<Dropdown
+								options={sessionOptions}
+								dropdownTitle={sessionTitle}
+							/>
+						</div>
+					</div>
+    </div>
   )
 }
 
