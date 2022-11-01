3e9f77457d7065dd4e4734e189eb070d1e24906b
Roy025 <prapti.roy025@gmail.com>
2022-11-02 01:02:50 +0600

    submit

--- frontend/src/Components/TeachersBill/Links.js
+++ frontend/src/Components/TeachersBill/Links.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { ReactDOM } from "react-dom";
-import { Link } from "react-router-dom";
-import "./Links.css";
+import { Link } from 'react-router-dom';
+import './Links.css';
 
 class Links extends React.Component {
 	constructor() {
@@ -25,52 +25,59 @@ class Links extends React.Component {
 		});
 	};
 
-  render() {
-    console.log(this.state);
-    return (
-      <>
-        {/* <nav className="Link col-md-2 this.state.show?'active':'hidden' "> */}
-        <div
-          // className={
-          //   this.state.show ? "Link col-md-2 active" : "Link col-md-2 hidden"
-          // }
-          className={this.state.show ? "Link active" : "Link hidden"}
-        >
-          {/* <ul>
+	render() {
+		// console.log(this.state);
+		return (
+			<>
+				{/* <nav className="Link col-md-2 this.state.show?'active':'hidden' "> */}
+				<div
+					// className={
+					//   this.state.show ? "Link col-md-2 active" : "Link col-md-2 hidden"
+					// }
+					className={this.state.show ? 'Link active' : 'Link hidden'}>
+					{/* <ul>
           <li> */}
-          {/* <Link className="list-group-item main-nav-link" to="/director"> */}
-          <Link className="rolelink" to="/director">
-            Director
-          </Link>
-          {/* </li>
+					{/* <Link className="list-group-item main-nav-link" to="/director"> */}
+					<Link
+						className="rolelink"
+						to="/director">
+						Director
+					</Link>
+					{/* </li>
           <li> */}
-          {/* <Link
+					{/* <Link
             className="list-group-item main-nav-link"
             to="/committee-chairman"
           > */}
-          <Link className="rolelink" to="/committee-chairman">
-            Committee Chairman
-          </Link>
-          {/* </li>
+					<Link
+						className="rolelink"
+						to="/committee-chairman">
+						Committee Chairman
+					</Link>
+					{/* </li>
           <li> */}
-          {/* <Link className="list-group-item main-nav-link" to="/teacher">
+					{/* <Link className="list-group-item main-nav-link" to="/teacher">
             Teacher
           </Link> */}
-          <Link className="rolelink" to="/teacher">
-            Teacher
-          </Link>
-          {/* </li>
+					<Link
+						className="rolelink"
+						to="/teacher">
+						Teacher
+					</Link>
+					{/* </li>
           <li> */}
-          {/* <Link
+					{/* <Link
             className="list-group-item main-nav-link"
             to="/chief-invigilator"
           >
             Chief Invigilator
           </Link> */}
-          <Link className="rolelink" to="/chief-invigilator">
-            Chief Invigilator
-          </Link>
-          {/* </li>
+					<Link
+						className="rolelink"
+						to="/chief-invigilator">
+						Chief Invigilator
+					</Link>
+					{/* </li>
         </ul> */}
 					{/* <div className="vr ms-auto" /> */}
 				</div>
