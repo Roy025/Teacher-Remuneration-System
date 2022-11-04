4982b92da279770969c14b0df746080fbce6169d
jowhermehajabin <jowherjuthi@gmail.com>
2022-11-04 21:29:21 +0600

    FinalBillDesign added

---
+++ frontend/src/Components/FinalBill/BillTable.js
@@ -0,0 +1,56 @@
+import "./FinalTeacherBill.css";
+
+function BillTable() {
+  const TotalAmount = "৳0";
+
+  const tableData = [
+    { criteria: "Question setting", course: "null", amount: 0 },
+    { criteria: "Question moderation", course: "null", amount: 0 },
+    { criteria: "Answerpaper checking", course: "null", amount: 0 },
+    { criteria: "Termtest", course: "null", amount: 0 },
+    { criteria: "Practical exam", course: "null", amount: 0 },
+    { criteria: "Tabulation", course: "null", amount: 0 },
+    { criteria: "Viva", course: "null", amount: 0 },
+    { criteria: "Scrutiny", course: "null", amount: 0 },
+    { criteria: "Chairman payment", course: "null", amount: 0 },
+    { criteria: "Question typing", course: "null", amount: 0 },
+    {
+      criteria: "Field work/ Project/ Term paper/ Seminar/ Monograph/ Thesis",
+      course: "null",
+      amount: 0,
+    },
+    { criteria: "Post expense", course: "null", amount: 0 },
+    { criteria: "Others", course: "null", amount: 0 },
+  ];
+
+  return (
+    <>
+      <table className="PaymentBillTable">
+        <thead>
+          <th>Criteria</th>
+          <th>Courses</th>
+          <th>Amount(BDT)</th>
+        </thead>
+        <tbody>
+          {tableData.map((data, index) => {
+            return (
+              <tr>
+                <td className="criteriaCol">{data.criteria}</td>
+                <td className="coursesCol">{data.course}</td>
+                <td className="amountCol">৳{data.amount}</td>
+              </tr>
+            );
+          })}
+          <tr>
+            <td colspan="2" className="netAmount">
+              Total
+            </td>
+            <td className="amountCol">{TotalAmount}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default BillTable;
--- frontend/src/Components/FinalBill/FinalTeacherBill.js
+++ frontend/src/Components/FinalBill/FinalTeacherBill.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import BillTable from "./BillTable";
+import "./FinalTeacherBill.css";
 
 function FinalTeacherBill() {
+  const session = "2018-19";
+  const semester = "2nd";
+  const department = "Software Engineering";
+  const teachername = "Ahsan Habib";
+  const teachertitle = "Assistant professor";
+  const teacheraddress = "Surma";
+
   return (
-    <div>
-      Bill
+    <div className="FullTablePage">
+      <div className="BillTitle">Teacher Remuneration Bill</div>
+      <div className="deptTitle">{`Department: ${department}`}</div>
+      <div className="smallTitle">
+        <p>
+          {`Session:
+ ${session}  Semester:
+ ${semester}`}
+        </p>
+      </div>
+      <div className="TeacherInfo">
+        <ol>
+          <li>{`Teacher's name: ${teachername}`}</li>
+          <li>{`Teacher's title: ${teachertitle}`}</li>
+          <li>{`Teacher's address: ${teacheraddress}`}</li>
+        </ol>
+      </div>
+      <div>
+        <BillTable />
+      </div>
     </div>
-  )
+  );
 }
 
-export default `FinalTeacherBill`
+export default FinalTeacherBill;
