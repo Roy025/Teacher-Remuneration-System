f3409ee79b5ffb89029acb47c3b4252008fd3aaf
Raisul191491 <aupiaupu@gmail.com>
2022-11-10 11:51:04 +0600

    Thesis comp push without func

--- frontend/src/Functions/ThesisComp.js
+++ frontend/src/Functions/ThesisComp.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ThesisTwoField from "./ThesisTwoField";
+import ThreeFields from "./ThreeFields";
 
 function ThesisComp({
   inputVivaBoard,
@@ -9,18 +11,90 @@ function ThesisComp({
   setThesisCoSuper,
   inputThesisAssess,
   setThesisAssess,
+  inputThesisManagers,
+  setThesisManagers,
+  localTeacherDB,
 }) {
+  const handleChangeCourse = (evnt, index) => {
+    const { name, value } = evnt.target;
+    const list = [...inputThesisManagers];
+    list[index][name] = value;
+    setThesisManagers(list);
+  };
+
   return (
     <div className="Container">
-      <div className="">
+      <div className="labelFlex TermpaperCompElement TermPaperCourse">
+        <label>Course ID</label>
         <input
           type="text"
-          name="name"
-          // onChange={(event) =>
-          //   handleChangeTeacher(event, index, ind)
-          // }
-          className=""
-          placeholder="Name"
+          name="course"
+          onChange={(evnt) => handleChangeCourse(evnt, 0)}
+          value={inputThesisManagers.course}
+          className="FormControl"
+          placeholder="Course"
+        />
+      </div>
+      <div className="formRow">
+        <label className="subLabel">Viva board</label>
+        <ThreeFields
+          inputFields={inputVivaBoard}
+          setInputFields={setVivaBoard}
+          localTeacherDB={localTeacherDB}
+        />
+      </div>
+
+      <div className="formRow">
+        <label className="subLabel">Thesis Superviser</label>
+        <ThreeFields
+          inputFields={inputThesisSuper}
+          setInputFields={setThesisSuper}
+          localTeacherDB={localTeacherDB}
+        />
+      </div>
+
+      <div className="formRow">
+        <label className="subLabel">Thesis Co-superviser</label>
+        <ThreeFields
+          inputFields={inputThesisCoSuper}
+          setInputFields={setThesisCoSuper}
+          localTeacherDB={localTeacherDB}
+        />
+      </div>
+
+      <div className="formRow">
+        <label className="subLabel">Thesis Assessor</label>
+        <ThreeFields
+          inputFields={inputThesisAssess}
+          setInputFields={setThesisAssess}
+          localTeacherDB={localTeacherDB}
+        />
+      </div>
+
+      <div className="formRow">
+        <label className="subLabel">Supervisor Assign</label>
+        <ThesisTwoField
+          inputFields={inputThesisManagers}
+          setInputFields={setThesisManagers}
+          field="super"
+        />
+      </div>
+
+      <div className="formRow">
+        <label className="subLabel">Co-superviser Assign</label>
+        <ThesisTwoField
+          inputFields={inputThesisManagers}
+          setInputFields={setThesisManagers}
+          field="cosuper"
+        />
+      </div>
+
+      <div className="formRow">
+        <label className="subLabel">Assessor Assign</label>
+        <ThesisTwoField
+          inputFields={inputThesisManagers}
+          setInputFields={setThesisManagers}
+          field="assess"
         />
       </div>
     </div>
