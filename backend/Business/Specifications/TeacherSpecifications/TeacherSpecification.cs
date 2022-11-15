63382df1361a248c09d22ede18c28f99174d5f52
Raisul191491 <aupiaupu@gmail.com>
2022-11-15 11:59:43 +0600

    TeacherEntity Update

--- backend/Business/Specifications/TeacherSpecifications/TeacherSpecification.cs
+++ backend/Business/Specifications/TeacherSpecifications/TeacherSpecification.cs
@@ -9,7 +9,7 @@ public class TeacherSpecification : BaseSpecification<Teacher>
         : base(
             x=>
             (string.IsNullOrEmpty(teacherParams.Email) || x.Email.ToLower() == teacherParams.Email) &&
-            (string.IsNullOrEmpty(teacherParams.Institute) || x.Institute == teacherParams.Institute) &&
+            // (string.IsNullOrEmpty(teacherParams.Institute) || x.Institute == teacherParams.Institute) &&
             (teacherParams.DepartmentId == null || x.DepartmentId == teacherParams.DepartmentId)
     )
     {
