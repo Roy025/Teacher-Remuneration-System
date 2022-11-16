1b1cd9f977ad0cf9ce8673911769cfdd8ed72515
Raisul191491 <aupiaupu@gmail.com>
2022-11-16 11:00:54 +0600

    Add Institute

--- frontend/src/Components/SampleDropdown/Dropdown.js
+++ frontend/src/Components/SampleDropdown/Dropdown.js
@@ -1,5 +1,4 @@
-
-import { useState } from "react";
+import React,{ useState } from "react";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./styles.css";
