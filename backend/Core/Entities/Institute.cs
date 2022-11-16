dd723cfb15fedf4e46bf034412c8cac73d357596
nihalshahria <nihalshahria@gmail.com>
2022-11-16 18:17:40 +0600

    Institute fetch functionality Update

--- backend/Core/Entities/Institute.cs
+++ backend/Core/Entities/Institute.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Core.Entities;
+[Index(nameof(Name), IsUnique = true)]
 public class Institute : BaseEntity
 {
     public string Name { get; set; }
