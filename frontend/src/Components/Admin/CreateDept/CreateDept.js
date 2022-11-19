32929b17dee1d525ab9ae264334466f0de3d1c74
Raisul191491 <aupiaupu@gmail.com>
2022-11-19 15:17:04 +0600

    add department complete

--- frontend/src/Components/Admin/CreateDept/CreateDept.js
+++ frontend/src/Components/Admin/CreateDept/CreateDept.js
@@ -8,6 +8,137 @@ const CreateDept = () => {
   // Store incoming data
   const [instituteList, setInstituteList] = useState([]);
   const [selectedInstitute, setSelectedInstitute] = useState([""]);
+
+  const [deptList, setDeptList] = useState([]);
+
+  const [data, setData] = useState({
+    id: "",
+    institute: "",
+    dept: "",
+  });
+
+  //Fetching department data
+  const fetchInstitute = async () => {
+    const response = await axios.get("/institute");
+    return response;
+  };
+
+  useQuery(["institution-list"], async () => {
+    const store = await fetchInstitute();
+    setInstituteList(store.data.data);
+    return store;
+  });
+
+  //Adding department
+
+  const addDept = async (info) => {
+    const response = await axios.post("/Admin/department", {
+      name: info.dept,
+      instituteId: info.id,
+    });
+    return response;
+  };
+  const { mutate, isError, error } = useMutation(addDept, {
+    onSuccess: (success) => {
+      console.log(success);
+      window.location.reload(false);
+    },
+  });
+  if (isError) {
+    console.log(error);
+  }
+
+  // //Fetching department data
+  let insTituteAvailabe = false;
+
+  const fetchDept = async () => {
+    const response = await axios.get(`/department`);
+    return response;
+  };
+
+  useQuery(
+    ["dept-list"],
+    async () => {
+      const store = await fetchDept(data.id);
+      setDeptList(store.data.data);
+      return store;
+    },
+    {
+      enabled: insTituteAvailabe,
+    }
+  );
+
+  //Handling data
+
+  const handleInstitute = (propName, option) => {
+    const newData = { ...data };
+    newData.propName = option.name;
+    newData.id = option.id;
+    insTituteAvailabe = true;
+    setData(newData);
+  };
+
+  const handleDept = (evnt) => {
+    const { name, value } = evnt.target;
+    const newData = { ...data };
+    newData.dept = value;
+    setData(newData);
+  };
+
+  return (
+    <div className="CreateDeptWrap">
+      <div className="CreateDeptContainer">
+        <h1 className="CreateDeptHeader">Add Department</h1>
+        <div className="AddInputFields">
+          <label className="AddLabel">Institute</label>
+          <DropdownNoTitleTeacher
+            options={instituteList}
+            propName="institute"
+            handleData={handleInstitute}
+            selected={selectedInstitute}
+            setSelected={setSelectedInstitute}
+          />
+        </div>
+        <div className="AddInputFields">
+          <label className="AddLabel">Department</label>
+          <input
+            type="text"
+            name="dept"
+            onChange={(evnt) => handleDept(evnt)}
+            value={data.dept}
+            className="AddInput"
+            placeholder="Department"
+          />
+        </div>
+        <button
+          className="AdminButton AdminSubmit"
+          onClick={() => mutate({ dept: data.dept, id: data.id })}
+        >
+          Submit
+        </button>
+      </div>
+
+      <div className="InstituteList">
+        <h2 className="AddHeader">Existing Department</h2>
+        {deptList.map((data, index) => {
+          return <div className="InstituteItem">{data.name}</div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CreateDept;
+import React, { useState, useEffect } from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { instance as axios } from "../../axios";
+import "./CreateDept.css";
+import DropdownNoTitleTeacher from "../../../Functions/DropdownNoTitleTeacher";
+
+const CreateDept = () => {
+  // Store incoming data
+  const [instituteList, setInstituteList] = useState([]);
+  const [selectedInstitute, setSelectedInstitute] = useState([""]);
   const [insTituteAvailabe, setInsTituteAvailabe] = useState(false);
 
   const [deptList, setDeptList] = useState([]);
