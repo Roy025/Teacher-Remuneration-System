154c15ff5c5ef48e5d38f0acf202bd171781623f
nihalshahria <nihalshahria@gmail.com>
2022-11-20 12:27:17 +0600

    Update Course Entity

--- backend/Core/Entities/Course.cs
+++ backend/Core/Entities/Course.cs
@@ -12,4 +12,5 @@ public class Course : BaseEntity
     public string Title { get; set; }
     public Decimal Credit { get; set; }
     public string Type { get; set; } // Theory or Lab
+    public string? Level { get; set; } = "Undergrad";
 }
