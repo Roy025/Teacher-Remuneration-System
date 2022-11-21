6ee876b81b03aa81448b6324d50585d09fe4384b
jowhermehajabin <jowherjuthi@gmail.com>
2022-11-21 16:36:14 +0600

    Final bill design and print function done

--- frontend/src/Components/FinalBill/FinalTeacherBill.js
+++ frontend/src/Components/FinalBill/FinalTeacherBill.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { ReactToPrint } from "react-to-print";
+import React, { useRef } from "react";
+import ReactToPrint from "react-to-print";
 import Dropdown, {
   semesterOptions,
   semesterTitle,
@@ -9,7 +9,7 @@ import Dropdown, {
 import BillTable from "./BillTable";
 import "./FinalTeacherBill.css";
 
-function FinalTeacherBill() {
+const FinalTeacherBill = () => {
   const session = "2018-19";
   const semester = "2nd";
   const teachername = "Ahsan Habib";
@@ -17,60 +17,59 @@ function FinalTeacherBill() {
   const teacherdepartment = "Software Engineering";
   const teacheraddress = "Surma";
 
+  let ref = useRef(null);
+
   return (
-    <div className="FullTablePage">
-      <div className="generateBillTitle">Generate Bill</div>
-      <div className="selectSessionSemester">
-        <div className="selectSessionSemesterChild">
-          <Dropdown options={sessionOptions} dropdownTitle={sessionTitle} />
-        </div>
-        <div className="selectSessionSemesterChild">
-          <Dropdown options={semesterOptions} dropdownTitle={semesterTitle} />
+    <div>
+      <div className="TablePageHeader">
+        <div className="generateBillTitle">Generate Bill</div>
+        <div className="selectSessionSemester">
+          <div className="selectSessionSemesterChild">
+            <Dropdown options={sessionOptions} dropdownTitle={sessionTitle} />
+          </div>
+          <div className="selectSessionSemesterChild">
+            <Dropdown options={semesterOptions} dropdownTitle={semesterTitle} />
+          </div>
         </div>
       </div>
 
-      <ReactToPrint
-        trigger={() => {
-          return <button>Print</button>;
-        }}
-        content={() => this.componentRef}
-        pagestyle="print"
-      />
-
-      <div
-        ref={(el) => {
-          this.componentRef = el;
-        }}
-        className="BillForm"
-      >
-        <div className="BillTitle">Teacher Remuneration Bill</div>
-        <div className="smallTitle">
-          <p>
-            <b>Session:</b>
-            {` ${session}`} <b>Semester:</b>
-            {` ${semester}`}
-          </p>
-        </div>
-        <div className="TeacherInfo">
-          <div>
-            <b>Teacher's name:</b> {` ${teachername}`}
+      <div className="BillForm">
+        <ReactToPrint
+          trigger={() => {
+            return <button className="printButton">Print</button>;
+          }}
+          content={() => ref}
+        />
+        <div ref={(el) => (ref = el)} className="Bill">
+          <div className="BillTitle">Teacher Remuneration Bill</div>
+          <div className="smallTitle">
+            <p>
+              <b>Session:</b>
+              {` ${session}`} <b>Semester:</b>
+              {` ${semester}`}
+            </p>
+          </div>
+          <div className="TeacherInfo">
+            <div>
+              <b>Teacher's name:</b> {` ${teachername}`}
+            </div>
+            <div>
+              <b>Teacher's title:</b> {` ${teachertitle}`}
+            </div>
+            <div>
+              <b>Teacher's department:</b> {` ${teacherdepartment}`}
+            </div>
+            <div>
+              <b>Teacher's address:</b> {` ${teacheraddress}`}
+            </div>
           </div>
           <div>
-            <b>Teacher's title:</b> {` ${teachertitle}`}
+            <BillTable />
           </div>
-          <div>
-            <b>Teacher's department:</b> {` ${teacherdepartment}`}
-          </div>
-          <div>
-            <b>Teacher's address:</b> {` ${teacheraddress}`}
-          </div>
-        </div>
-        <div>
-          <BillTable />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default FinalTeacherBill;
