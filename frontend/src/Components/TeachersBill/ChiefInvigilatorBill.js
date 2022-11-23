aac892bb59f335b35644095cfd76f22386951a5e
Raisul191491 <aupiaupu@gmail.com>
2022-11-23 15:03:47 +0600

    Final bill complete UI

--- frontend/src/Components/TeachersBill/ChiefInvigilatorBill.js
+++ frontend/src/Components/TeachersBill/ChiefInvigilatorBill.js
@@ -1,70 +1,70 @@
-import React, {useState} from 'react';
-import Links from "./Links";
-import "./TeachersBill.css";
-import "./FormButton.css";
+// import React, {useState} from 'react';
+// import Links from "./Links";
+// import "./TeachersBill.css";
+// import "./FormButton.css";
 
-import Dropdown, {
-  semesterOptions,
-  semesterTitle,
-  sessionTitle,
-  sessionOptions,
-} from "../SampleDropdown/Dropdown";
-import "../SampleDropdown/styles.css";
-import InvigilationComp from "./InvigilationComp";
+// import Dropdown, {
+//   semesterOptions,
+//   semesterTitle,
+//   sessionTitle,
+//   sessionOptions,
+// } from "../SampleDropdown/Dropdown";
+// import "../SampleDropdown/styles.css";
+// import InvigilationComp from "./InvigilationComp";
 
-const ChiefInvigilatorBill = () => {
-  const [invigilation, setInvigilation] = useState([
-    {
-      course: "",
-      names: [""],
-    },
-  ]);
+// const ChiefInvigilatorBill = () => {
+//   const [invigilation, setInvigilation] = useState([
+//     {
+//       course: "",
+//       names: [""],
+//     },
+//   ]);
 
-  const [exam, setExam] = useState({
-    session: "",
-    semester: "",
-  });
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  const submit = async () => {
-    console.log(exam);
-  };
-  return (
-    <>
-      <Links />
-      <div className="FullFormPage">
-        <form className="Form" onSubmit={handleSubmit}>
-          <div className="DropdownformRow">
-            <div className="FormSubRow">
-              <Dropdown
-                options={sessionOptions}
-                dropdownTitle={sessionTitle}
-                handleData={(child) => HandleSem(child, exam, setExam)}
-              />
-            </div>
-            <div className="FormSubRow">
-              <Dropdown
-                options={semesterOptions}
-                dropdownTitle={semesterTitle}
-                handleData={(child) => HandleSem(child, exam, setExam)}
-              />
-            </div>
-          </div>
+//   const [exam, setExam] = useState({
+//     session: "",
+//     semester: "",
+//   });
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//   };
+//   const submit = async () => {
+//     console.log(exam);
+//   };
+//   return (
+//     <>
+//       <Links />
+//       <div className="FullFormPage">
+//         <form className="Form" onSubmit={handleSubmit}>
+//           <div className="DropdownformRow">
+//             <div className="FormSubRow">
+//               <Dropdown
+//                 options={sessionOptions}
+//                 dropdownTitle={sessionTitle}
+//                 handleData={(child) => HandleSem(child, exam, setExam)}
+//               />
+//             </div>
+//             <div className="FormSubRow">
+//               <Dropdown
+//                 options={semesterOptions}
+//                 dropdownTitle={semesterTitle}
+//                 handleData={(child) => HandleSem(child, exam, setExam)}
+//               />
+//             </div>
+//           </div>
 
-          <div className="formRow">
-            <label className="Label">Invigilation</label>
-            <InvigilationComp tag={"Invigilator's name"} invigilation={invigilation} setInvigilation={setInvigilation}/>
-          </div>
-          <div className="formRow SubmitRow">
-            <button type="submit" className="submitButton" onClick={submit}>
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
-  );
-};
+//           <div className="formRow">
+//             <label className="Label">Invigilation</label>
+//             <InvigilationComp tag={"Invigilator's name"} invigilation={invigilation} setInvigilation={setInvigilation}/>
+//           </div>
+//           <div className="formRow SubmitRow">
+//             <button type="submit" className="submitButton" onClick={submit}>
+//               Submit
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </>
+//   );
+// };
 
-export default ChiefInvigilatorBill;
+// export default ChiefInvigilatorBill;
